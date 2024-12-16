d8ed06087adf6a0a7cadf10c48137f9ca629756d
riku103 <riku311n@gmail.com>
2024-12-16 22:35:57 +0900

    Hero コンポーネントを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+
 function App() {
   return (
     <div className="App">
       <Header />
 
       <main>
-        <section id="hero">
-          <div className="section-container">
-            <h1>新しい体験をあなたに</h1>
-            <p>革新的な製品で、あなたの日常をより豊かに、より便利に。</p>
-            <a href="#contact" className="cta-button">今すぐ体験する</a>
-          </div>
-        </section>
+        <Hero />
 
         <section id="features">
           <div className="section-container">
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div className="section-container">
+        <h1>新しい体験をあなたに</h1>
+        <p>革新的な製品で、あなたの日常をより豊かに、より便利に。</p>
+        <a href="#contact" className="cta-button">今すぐ体験する</a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
