49c91759db6d01834cbaa48b57ccac06eac9dc39
riku103 <riku311n@gmail.com>
2024-12-17 22:36:42 +0900

    Header の CSS Modules を追加

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,33 @@
+import React from "react";
+import styles from "./Header.module.css";
+
 const Header = () => {
+  const handleHamburgerClick = () => {
+    const navLinks = document.querySelector(`.${styles.navLinks}`);
+    navLinks.classList.toggle(styles.active);
+  };
+
   return (
-    <header>
-      <div className="header-container">
-        <div className="logo">My Stylish LP</div>
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.logo}>My Stylish LP</div>
         <nav>
-          <div className="hamburger">☰</div>
-          <ul className="nav-links">
-            <li><a href="#hero">ホーム</a></li>
-            <li><a href="#features">特徴</a></li>
-            <li><a href="#testimonials">お客様の声</a></li>
-            <li><a href="#contact">お問い合わせ</a></li>
+          <div className={styles.hamburger} onClick={handleHamburgerClick}>
+            ☰
+          </div>
+          <ul className={`${styles.navLinks}`}>
+            <li>
+              <a href="#hero">ホーム</a>
+            </li>
+            <li>
+              <a href="#features">特徴</a>
+            </li>
+            <li>
+              <a href="#testimonials">お客様の声</a>
+            </li>
+            <li>
+              <a href="#contact">お問い合わせ</a>
+            </li>
           </ul>
         </nav>
       </div>
