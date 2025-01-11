11eaf6bc0d10571836bcbcd6caae64b1b888ff07
riku103 <riku311n@gmail.com>
2025-01-11 22:17:45 +0900

    features コンポーネントに props を設定

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,26 +1,39 @@
 import styles from './Features.module.css';
 
-const Features = () => {
+const getIconName = (index) => {
+  const icons = ['gem', 'mobile-alt', 'headset'];
+  return icons[index];
+};
+
+const Features = ({
+  title = '特徴',
+  features = [
+    {
+      title: "最高品質",
+      description: "厳選された素材と職人技が生み出す、比類なき品質をお届けします。"
+    },
+    {
+      title: "使いやすさ",
+      description: "直感的なデザインで、誰もが簡単に使いこなせる製品です。"
+    },
+    {
+      title: "24時間サポート",
+      description: "お客様のニーズに合わせて、いつでもどこでもサポートいたします。"
+    },
+  ]
+}) => {
   return (
     <section id="features">
       <div className={styles.sectionContainer}>
-        <h2>特徴</h2>
+        <h2>{title}</h2>
         <div className={styles.featureContainer}>
-          <div className={styles.feature}>
-            <i className="fas fa-gem"></i>
-            <h3>最高品質</h3>
-            <p>厳選された素材と職人技が生み出す、比類なき品質をお届けします。</p>
-          </div>
-          <div className={styles.feature}>
-            <i className="fas fa-mobile-alt"></i>
-            <h3>使いやすさ</h3>
-            <p>直感的なデザインで、誰もが簡単に使いこなせる製品です。</p>
-          </div>
-          <div className={styles.feature}>
-            <i className="fas fa-headset"></i>
-            <h3>24時間サポート</h3>
-            <p>お客様のニーズに合わせて、いつでもどこでもサポートいたします。</p>
-          </div>
+          {features.map((feature, index) => (
+            <div key={index} className={styles.feature}>
+              <i className={`fas fa-${getIconName(index)}`}></i>
+              <h3>{feature.title}</h3>
+              <p>{feature.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
