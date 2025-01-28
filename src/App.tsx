aea6f44e643abe033bda0c479607411be2595fe3
riku103 <riku311n@gmail.com>
2025-01-28 20:55:15 +0900

    About コンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/components/About/About.tsx
@@ -0,0 +1,11 @@
+// アバウトページのコンポーネント
+function About() {
+  return (
+    <div>
+      <h1>アバウトページ</h1>
+      <p>こちらはアバウトページです。</p>
+    </div>
+  );
+}
+
+export default About;
