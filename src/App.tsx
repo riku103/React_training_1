b305d768b4b8cb585fa18963e94ba029037f1466
riku103 <riku311n@gmail.com>
2025-01-29 19:27:19 +0900

    NotFoundコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import About from './components/About/About';
-
+import NotFound from './components/NotFound';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
